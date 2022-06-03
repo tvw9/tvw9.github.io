2de0291d55b4bce88510f0ac463ba6ad4589bacf
tvw9 <106173931+tvw9@users.noreply.github.com>
2022-06-03 13:53:19 +0500

    yees

---
+++ taskmanager/main/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+#from django.db.models import CharField
+#xyz1234test is password
+#testadmin1 is nickname
+
+
+class Task(models.Model):
+    title = models.CharField('Название', max_length=50)
+    task = models.TextField('Описание')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
